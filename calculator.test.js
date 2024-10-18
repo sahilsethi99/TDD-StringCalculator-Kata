964c72daac03780acd5bb13aa26bcce8bd8ae0bd
sahilsethi99 <68042681+sahilsethi99@users.noreply.github.com>
2024-10-19 01:31:58 +0530

    Step 2.1: Write a failing test for single number input in 'add' function

--- calculator.test.js
+++ calculator.test.js
@@ -3,3 +3,7 @@ const {add }= require('./calculator');  // assuming our function will be in a fi
 test('should return 0 when an empty string is passed', () => {
     expect(add("")).toBe(0);
 });
+
+test('should return the number itself when a single number is passed', () => {
+    expect(add("1")).toBe(1);
+});
