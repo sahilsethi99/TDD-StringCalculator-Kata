8254edf4db749fe87c9c8e596edbb7c65081edba
sahilsethi99 <68042681+sahilsethi99@users.noreply.github.com>
2024-10-19 01:41:35 +0530

    Step 5.1: Write a failing test for supporting different delimiters in 'add' function

--- calculator.test.js
+++ calculator.test.js
@@ -15,3 +15,7 @@ test('should return the sum of two comma-separated numbers', () => {
 test('should return the sum when newlines are used as delimiters', () => {
     expect(add("1\n2,3")).toBe(6);
 });
+
+test('should return the sum when a custom delimiter is used', () => {
+    expect(add("//;\n1;2;3")).toBe(6);
+});
