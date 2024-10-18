ea7c6f5766e47847596bac10c9d46c4fd63053bc
sahilsethi99 <68042681+sahilsethi99@users.noreply.github.com>
2024-10-19 01:33:22 +0530

    Step 2.2: Implement code to handle single number input in 'add' function (passing test)

--- calculator.js
+++ calculator.js
@@ -2,6 +2,10 @@ function add(numbers) {
     if (numbers === "") {
         return 0;
     }
+
+    if (!isNaN(numbers)) {
+        return parseInt(numbers, 10);  // handle single number input
+    }
 }
 
 module.exports = { add };
